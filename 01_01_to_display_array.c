cf6c8e12ecf90af3bb2b635daf3f274e77dfa4b0
Piyush096 <mishra.piyush1886@gmail.com>
2022-01-01 21:55:08 +0530

    Update C

--- 01_01_to_display_array.c
+++ 01_01_to_display_array.c
@@ -3,19 +3,25 @@
 
 int main(void)
 {
-    int n;
+    int n,m;
     printf("Enter the size of array: ");
-    scanf("%d", &n);
+    scanf("%d %d", &n,&m);
 
-    int array[n];
+    printf("Enter the element of array: ");
+    int array[n][m];
     for (int i = 0; i < n; i++)
-    {
-        printf("Enter the value of array[%d]: ", i);
-        scanf("%d", &array[i]);
-    }
+        for (int j = 0; j < m; j++)
+            scanf("%d", &array[i][j]);
+
 
     printf("The elements of array are :\n ");
-    for (int i = 0 ; i < n ; i++)
-        printf("%d ", array[i]);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d", array[i][j]);
+        }
     printf("\n");
+    }
+
 }
